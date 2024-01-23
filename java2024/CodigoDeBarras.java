ed309a5ab74f10209d72c57d8ca69987bc2bd9b9
ImZiki <98941902+ImZiki@users.noreply.github.com>
2024-01-23 12:12:30 +0100

    Actualizar CodigoDeBarras.java

--- java2024/CodigoDeBarras.java
+++ java2024/CodigoDeBarras.java
@@ -11,43 +11,41 @@ public static void main(String[] args) {
             int digit = codigo.charAt(len - 2 - i) - 48;
             valor += (i % 2 == 0) ? 3 * digit : digit;
         }
-        boolean correcto = (valor + (codigo.charAt(len - 1) - 48)) % 10 == 0;
+        
+
+
+boolean correcto = (valor + (codigo.charAt(len - 1) - 48)) % 10 == 0;
         System.out.print(correcto ? "SI" : "NO");
         if (codigo.length() > 8 && correcto) {
             if (codigo.length() < 13) {
                 System.out.print(" EEUU");
             } else {
                 String codigoPais = codigo.substring(0, 3);
-                String pais = getPais(codigoPais);
+                String pais = "Desconocido";
+            if (codigo.startsWith("0")) {
+               pais =  "EEUU";
+            } else if (codigo.startsWith("380")) {
+               pais =  "Bulgaria";
+            } else if (codigo.startsWith("50")) {
+               pais =  "Inglaterra";
+            } else if (codigo.startsWith("539")) {
+               pais =  "Irlanda";
+            } else if (codigo.startsWith("560")) {
+               pais =  "Portugal";
+            } else if (codigo.startsWith("70")) {
+               pais =  "Noruega";
+            } else if (codigo.startsWith("759")) {
+               pais =  "Venezuela";
+            } else if (codigo.startsWith("850")) {
+               pais =  "Cuba";
+            } else if (codigo.startsWith("890")) {
+               pais =  "India";
+            }
                 System.out.print(pais != "Desconocido" ? " " + pais : " Desconocido");
             }
         }
         System.out.println();
         codigo = sc.next();
     }
-}
-
-    private static String getPais(String codigo) {
-        String pais = "Desconocido";
-        if (codigo.startsWith("0")) {
-        pais =  "EEUU";
-        } else if (codigo.startsWith("380")) {
-            pais =  "Bulgaria";
-        } else if (codigo.startsWith("50")) {
-            pais =  "Inglaterra";
-        } else if (codigo.startsWith("539")) {
-            pais =  "Irlanda";
-        } else if (codigo.startsWith("560")) {
-            pais =  "Portugal";
-        } else if (codigo.startsWith("70")) {
-            pais =  "Noruega";
-        } else if (codigo.startsWith("759")) {
-            pais =  "Venezuela";
-        } else if (codigo.startsWith("850")) {
-            pais =  "Cuba";
-        } else if (codigo.startsWith("890")) {
-            pais =  "India";
-        }
-        return pais;
-}
+}
 }

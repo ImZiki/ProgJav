2493dca65626fab118f9a80b43f0a528b5e070e0
ImZiki <98941902+ImZiki@users.noreply.github.com>
2024-01-25 00:16:11 +0100

    Update Carta.java

--- ejercicios/Carta.java
+++ ejercicios/Carta.java
@@ -44,14 +44,102 @@ public class Carta {
         }
     }
     
-    public void mostrarCarta(){
-        System.out.printf("%d de %c", numero, palo);
+    public void mostrarCarta() {
+        String nombreNumero;
+        String nombrePalo;
+        switch (numero) {
+            case 1:
+                nombreNumero = "As";
+                break;
+            case 11:
+                nombreNumero = "Jack";
+                break;
+            case 12:
+                nombreNumero = "Reina";
+                break;
+            case 13:
+                nombreNumero = "Rey";
+                break;
+            default:
+                nombreNumero = this.numero;
+                break;
+        }
+
+        
+        switch (palo) {
+            case 'c':
+                nombrePalo = "Corazones";
+                break;
+            case 'p':
+                nombrePalo = "Picas";
+                break;
+            case 'r':
+                nombrePalo = "Rombos";
+                break;
+            case 't':
+                nombrePalo = "Tréboles";
+                break;
+            default:
+                nombrePalo = "";
+                break;
+        }
+
+        System.out.println("Es el " + nombreNumero + " de " + nombrePalo);
     }
-    private int comparar(Carta carta){
+    private int comparar(Carta carta) {
         int resultado;
-        if(this.numero == carta.getNumero())
-            if(this.palo == carta.getPalo())
-        else if(this )
+
+        if (this.numero > carta.numero) {
+            resultado = -1; // La carta actual gana
+        } else if (this.numero < carta.numero) {
+            resultado = 1; // La carta pasada por parámetro gana
+        } else {
+            // Los números son iguales, comparar palos
+            String palos = "ctrp"; // Orden de victoria: Trébol, Rombos, Picas, Corazones
+            int indiceActual = palos.indexOf(this.palo);
+            int indiceOtra = palos.indexOf(carta.palo);
+
+            if (indiceActual < indiceOtra) {
+                resultado = -1; 
+            } else if (indiceActual > indiceOtra) {
+                resultado = 1; 
+            } else {
+                resultado = 0; 
+            }
+        }
+
+        return resultado;
     }
-    
+    public void mostrarGanadora(Carta carta) {
+        int resultado = comparar(carta);
+
+        if (resultado == -1) {
+            System.out.println("La carta actual gana a la carta pasada por parámetro.");
+        } else if (resultado == 1) {
+            System.out.println("La carta pasada por parámetro gana a la carta actual.");
+        } else {
+            System.out.println("Hay un empate entre las dos cartas.");
+        }
+    }
+ public static void main(String[] args) {
+        Carta carta1 = new Carta();
+        Carta carta2 = new Carta(carta1);
+
+        carta1.setNumero(1);
+        carta1.setPalo('c');
+
+        carta1.mostrarCarta();
+        carta2.mostrarCarta();
+        carta1.mostrarGanadora(carta2);
+        System.out.println();
+
+        carta2.setNumero(2);
+        carta2.setPalo('t');
+     
+        carta1.mostrarCarta();
+        carta2.mostrarCarta();
+        System.out.println();
+        carta1.mostrarGanadora(carta2);
+
+    }   
 }
